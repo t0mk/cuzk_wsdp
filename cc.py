4b631ae654d800a584dba8610313be7e9ba8e15d
Tomas Karasek <tom.to.the.k@gmail.com>
2020-08-23 16:10:08 +0200

    ff

--- cc.py
+++ cc.py
@@ -5,51 +5,55 @@ from zeep.cache import SqliteCache
 from zeep.transports import Transport
 from zeep.wsse.username import UsernameToken
 
-wsdl = "https://wsdptrial.cuzk.cz/trial/dokumentace/ws28/wsdp/vyhledat_v28.wsdl"
-transport = Transport(cache=SqliteCache())
-
-
-
-settings = Settings(raw_response=False, strict=False, xml_huge_tree=True)
-settings = Settings(strict=False, xml_huge_tree=True)
-
-vyhC = Client(wsdl,
-    transport=transport,
-    wsse=UsernameToken("WSTEST","WSHESLO"),
-    settings=settings,
-)
-
-
-kuid = '693936'
-ps = [
-    [kuid, '76', '2'],
-    [kuid, '41', '1'],
-    [kuid, '104', ''],
-    [kuid, '127', ''],
-    [kuid, '129', ''],
-    [kuid, '130', ''],
-    [kuid, '1191', ''],
-    [kuid, '1192', ''],
-    [kuid, '1362', ''],
-    [kuid, '1385', '8'],
-    [kuid, '1385', '9'],
-    [kuid, '1385', '11'],
-    [kuid, '2275', '1'],
+KUID = '693936'
+PS = [
+    [KUID, '76', '2'],
+    [KUID, '41', '1'],
+    [KUID, '104', ''],
+    [KUID, '127', ''],
+    [KUID, '129', ''],
+    [KUID, '130', ''],
+    [KUID, '1191', ''],
+    [KUID, '1192', ''],
+    [KUID, '1362', ''],
+    [KUID, '1385', '8'],
+    [KUID, '1385', '9'],
+    [KUID, '1385', '11'],
+    [KUID, '2275', '1'],
 ] 
 
+trialWsdls = {
+    "vyhledat": "https://wsdptrial.cuzk.cz/trial/dokumentace/ws28/wsdp/vyhledat_v28.wsdl",
+    "informace": "https://wsdptrial.cuzk.cz/trial/dokumentace/ws28/wsdp/informace_v28.wsdl",
+    "sestavy": "https://wsdptrial.cuzk.cz/trial/dokumentace/ws28/wsdp/sestavy_v28.wsdl",
+    "ciselnik": "https://wsdptrial.cuzk.cz/trial/dokumentace/ws28/wsdp/ciselnik_v28.wsdl",
+    "ucet": "https://wsdptrial.cuzk.cz/trial/dokumentace/ws28/wsdp/ucet_v28.wsdl",
+}
+
+
+def TrialClient(wsdl):
+    transport = Transport(cache=SqliteCache())
+    settings = Settings(raw_response=False, strict=False, xml_huge_tree=True)
+    settings = Settings(strict=False, xml_huge_tree=True)
+
+    return Client(trialWsdls[wsdl],
+        transport=transport,
+        wsse=UsernameToken("WSTEST","WSHESLO"),
+        settings=settings,
+    )
+
+
+
 def pp(kuk, kc, pd):
+    c = TrialClient("vyhledat")
     print(kuk, kc, pd)
+    args = {
+        "katastrUzemiKod": kuk,
+        "kmenoveCislo": kc,
+    }
     if pd:
-        resp = vyhC.service.najdiParcelu(
-            katastrUzemiKod=kuk,
-            kmenoveCislo=kc,
-            poddeleni=pd,
-        )
-    else:
-        resp = vyhC.service.najdiParcelu(
-            katastrUzemiKod=kuk,
-            kmenoveCislo=kc,
-        )
+        args['poddeleni'] = pd
+    resp = c.service.najdiParcelu(**args)
     return resp
 
     #dom = xml.dom.minidom.parseString(out)
@@ -57,7 +61,7 @@ def pp(kuk, kc, pd):
     #print(out)
 
 def test_pp():
-    for p in ps:
+    for p in PS:
         r = pp(p[0], p[1], p[2])
         print(r)
 
